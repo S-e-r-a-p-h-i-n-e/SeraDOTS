714f879fb8eed2a2ad062949ed80de9e090ed732
Andre <andrelouiepascual@proton.me>
2026-02-23 18:27:17 +0800

    clarified env loader & renamed env override files

--- core/env/env.sh
+++ core/env/env.sh
@@ -1,7 +1,7 @@
 # SeraDOTS environment loader
 
-ENV_DIR="$HOME/.config/YASD/env"
+ENV_DIR="$HOME/.config/SeraDOTS/env"
 
 [ -f "$ENV_DIR/00-core.env" ] && . "$ENV_DIR/00-core.env"
-[ -f "$ENV_DIR/20-toolkits.env" ] && . "$ENV_DIR/20-toolkits.env"
-[ -f "$ENV_DIR/90-user-prefs.sh" ] && . "$ENV_DIR/90-user-prefs.sh"
+[ -f "$ENV_DIR/01-toolkits.env" ] && . "$ENV_DIR/01-toolkits.env"
+[ -f "$ENV_DIR/99-user-prefs.sh" ] && . "$ENV_DIR/99-user-prefs.sh"
